0680aab61878e99732e3332063915851555767fa
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-23 05:06:00 -0700

    composer-char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  console.log("let's rock");
+  const textarea = $('.new-tweet form textarea');
+  textarea.on('input', function(event) {
+    const textareaValue = $(this).val();
+    const textareaLength = 140 - textareaValue.length;
+
+    const counter = $(this).parent().find('.counter');
+    counter.text(Math.abs(textareaLength));
+
+    if (textareaLength < 0 && counter.css('color') !== 'rgb(255, 0, 0)') {
+      counter.css('color', 'red');
+    } else if (textareaLength >= 0 && counter.css('color') !== 'rgb(84, 81, 73)') {
+      counter.css('color', 'rgb(84, 81, 73)');
+    }
+
+  });
+
+});
+
--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -31,6 +31,19 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+app.post("/tweets/", (req, res) => {
+  res.send('start');
+  // let tweet_content = {};
+  // tweet_content["text"] = req.body.text;
+  // console.log(content);
+  // db.tweets[db.tweets.length + 1].user = { user };
+  // db.tweets[db.tweets.length + 1].content = { content };
+  // db.tweets[db.tweets.length + 1].created_at = Date(now());
+
+  // res.redirect(`/tweets/`);
+});
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
